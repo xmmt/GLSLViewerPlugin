aeec68b9869e6c0400e88b96569211d9c7e31930
xmmt <mkmax472@gmail.com>
2019-01-17 08:03:26 +0300

    Add OpenGL (jogamp)

--- src/main/java/MyToolWindow.java
+++ src/main/java/MyToolWindow.java
@@ -4,14 +4,53 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.image.ImageObserver;
 
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLEventListener;
+import com.jogamp.opengl.GLProfile;
+import com.jogamp.opengl.GLCapabilities;
+import com.jogamp.opengl.awt.GLJPanel;
+import javax.swing.JFrame;
+
+import java.awt.BorderLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 public class MyToolWindow {
     private JPanel myToolWindowContent;
 
     public MyToolWindow(ToolWindow toolWindow) {
         myToolWindowContent = new JPanel();
         myToolWindowContent.setLayout(new BorderLayout());
-        JButton northButton = new JButton("North");
-        myToolWindowContent.add(northButton, BorderLayout.NORTH);
+
+        GLProfile glprofile = GLProfile.getDefault();
+        GLCapabilities glcapabilities = new GLCapabilities( glprofile );
+        GLJPanel gljpanel = new GLJPanel( glcapabilities );
+
+        gljpanel.addGLEventListener( new GLEventListener() {
+
+            @Override
+            public void reshape( GLAutoDrawable glautodrawable, int x, int y, int width, int height ) {
+                OneTriangle.setup( glautodrawable.getGL().getGL2(), width, height );
+            }
+
+            @Override
+            public void init( GLAutoDrawable glautodrawable ) {
+            }
+
+            @Override
+            public void dispose( GLAutoDrawable glautodrawable ) {
+            }
+
+            @Override
+            public void display( GLAutoDrawable glautodrawable ) {
+                OneTriangle.render( glautodrawable.getGL().getGL2(), glautodrawable.getSurfaceWidth(), glautodrawable.getSurfaceHeight() );
+            }
+        });
+
+        myToolWindowContent.add( gljpanel, BorderLayout.CENTER );
+
+        //JButton northButton = new JButton("North");
+        //myToolWindowContent.add(northButton, BorderLayout.NORTH);
     }
 
     public JPanel getContent() {
