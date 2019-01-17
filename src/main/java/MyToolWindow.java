c6cec7e7ccb2de2fd9679395edc22d8d421f13a0
xmmt <mkmax472@gmail.com>
2019-01-17 03:11:15 +0300

    Created MyToolWindow

--- src/main/java/MyToolWindow.java
+++ src/main/java/MyToolWindow.java
@@ -1,11 +1,18 @@
 import com.intellij.openapi.wm.ToolWindow;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.ImageObserver;
 
 public class MyToolWindow {
     private JPanel myToolWindowContent;
 
-    public MyToolWindow(ToolWindow toolWindow) {}
+    public MyToolWindow(ToolWindow toolWindow) {
+        myToolWindowContent = new JPanel();
+        myToolWindowContent.setLayout(new BorderLayout());
+        JButton northButton = new JButton("North");
+        myToolWindowContent.add(northButton, BorderLayout.NORTH);
+    }
 
     public JPanel getContent() {
         return myToolWindowContent;
